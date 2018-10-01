930503eb4136c0afeb478cc0d3d4b307a8d3399a
ivaylostanev <ivailo.s.stanev@gmail.com>
2018-10-01 17:56:27 +0300

    MyArrayList completed

--- src/main/java/MyArrayList.java
+++ src/main/java/MyArrayList.java
@@ -1,6 +1,183 @@
-public class MyArrayList {
+import java.util.*;
+
+public class MyArrayList<E> implements List<E> {
+
     public static void main(String[] args) {
-        System.out.println("asdsad");
-        System.out.println("branch f1");
+        List<String> ls = new MyArrayList<>();
+
+        ls.add("Jim");
+        ls.add("Jim");
+        ls.add("Jim");
+        ls.add("Jim");
+        ls.add("Jim");
+
+        System.out.println(ls.toString());
+    }
+
+    @SuppressWarnings("unchecked")
+    private E[] data = (E[]) (new Object[10]);
+    private int count = 0;
+
+    @Override
+    public int size() {
+        return count;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return count != 0;
+    }
+
+    private void ensureCapacity(int minCapacity){
+        if (data.length < minCapacity) {
+            data = Arrays.copyOf(data, minCapacity);
+        }
+    }
+
+    @Override
+    public boolean add(E e) {
+        data[count++] = e;
+        return true;
+    }
+
+    @Override
+    public void clear() {
+        Arrays.fill(data, 0, count, null);
+        count = 0;
+    }
+
+    @Override
+    public E get(int index) {
+        if (index < count && index > 0) {
+            return data[index];
+        } else {
+            throw new IndexOutOfBoundsException(index + " is invalid!");
+        }
+    }
+
+    @Override
+    public E set(int index, E element) {
+        if (index > count && index < 0) {
+            E rv = data[index];
+            data[index] = element;
+            return rv;
+        } else {
+            throw new IndexOutOfBoundsException(index + " is invalid!");
+        }
+    }
+
+    @Override
+    public void add(int index, E element) {
+        if (index > count || index < 0) {
+            throw new IndexOutOfBoundsException(index + " is invalid!");
+        }
+        if (index == count) {
+            add(element);
+            return;
+        }
+        ensureCapacity(count + 1);
+//        for (int i = count; i > index;) {
+//            data[i] = data[--i];
+//        }
+        System.arraycopy(data, index, data, index + 1, count - index);
+        data[index] = element;
+        count++;
+    }
+
+    @Override
+    public E remove(int index) {
+        E rv = get(index);
+        System.arraycopy(data, index + 1, data, index, count - index - 1);
+        data[count--] = null;
+        return rv;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("MyArrayList [");
+        for (int i = 0; i < count ; i++) {
+            sb.append(data[i]).append(", ");
+        }
+        if (count > 0) {
+         sb.setLength(sb.length() - 2);
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        return null;
     }
 }
